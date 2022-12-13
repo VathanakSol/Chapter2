0e621af5189f7fa05ee85b37ce1fd88d8306fdbc
Kim Uy Computer <Kim Uy Computer@VATHANAK>
2022-12-13 18:05:48 +0700

    Add project files.

---
+++ Chapter2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Chapter2;
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.Clear();
+        string st = "34";
+        double a = double.Parse(st);
+        Console.WriteLine(a);
+       
+        Console.WriteLine("a: {0}", a);
+        Console.WriteLine($"a: {a}");
+
+        int n = int.Parse(st);
+        int m = Convert.ToInt32(st);
+        Console.WriteLine(n);
+        Console.WriteLine(m);
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Chapter2/UserLogin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Chapter2;
+
+class UserLogin
+{
+    static void Mainx(String[] args)
+    {
+        Console.Clear();
+        string name, pass;
+        
+        Console.Write("Enter username: ");
+        name = Console.ReadLine();
+        Console.Write("Enter password: ");
+        pass = Console.ReadLine();
+        if (name == "Vathanak")
+        {
+            if (pass == "abc123")
+            {
+                Console.WriteLine("Verified your own account");
+                int a, b, result;
+                Console.Write("Input A: ");
+                a = int.Parse(Console.ReadLine());
+                Console.Write("Input B: ");
+                b = int.Parse(Console.ReadLine());
+                result = a + b;
+                Console.Write("The result: " + result);
+            }
+            else
+            {
+                Console.Write("Invalid account!");
+            }
+        }
+        else
+        {
+            Console.Write("Invalid account");
+        }
+        Console.ReadKey();
+    }
+
+
+        
+}
+
